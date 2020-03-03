3c82050b312bc28c2616ae2e9c766fba0d6dcd60
Mason Qian <Thunderpurtz@Masons-MacBook-Pro-2.local>
2020-03-02 22:19:10 -0800

    udp stuff

--- assignment1/udp/udp_client.py
+++ assignment1/udp/udp_client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 
 UDP_IP = '127.0.0.1'
@@ -7,15 +8,59 @@ BUFFER_SIZE = 1024
 MESSAGE = "ping"
 
 
+upload = []
+with open("upload.txt", "r") as up:
+    for line in up:
+        upload.append(line)
+
 def send(id=0):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto(f"{id}:{MESSAGE}".encode(), (UDP_IP, UDP_PORT))
-        data, ip = s.recvfrom(BUFFER_SIZE)
-        print("received data: {}: {}".format(ip, data.decode()))
-    except socket.error:
-        print("Error! {}".format(socket.error))
-        exit()
+    socket_timeouts = 0
+    retry = 0 #attempts to send data
+    resend = 0 #attempts to send data without packet loss
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(2.5)
+    packet = 0
+    while True:
+        try:
+            print("packet in try=", packet)
+            #s.sendto(f"{id}:{MESSAGE}".encode(), (UDP_IP, UDP_PORT))
+            s.sendto(f"{id}:{packet}:{upload[0]}".encode(), (UDP_IP, UDP_PORT))
+            print("sent data")
+            data, ip = s.recvfrom(BUFFER_SIZE)
+
+            #didnt receive an acknowledgment
+            if not data:
+                retry += 1
+                if retry == 5:
+                    print("Server not responding after repeated attempts. Exiting client...")
+                    exit()
+                continue
+            elif data.decode(encoding="utf-8)").strip() == "resend":
+                resend += 1
+                if resend == 10:
+                    print("max attempts to send data without packet loss reached, exiting")
+                    exit()
+            else: #acknowledge
+                print("acknowledge")
+                del upload[0]
+                packet += 1
+                print("packet in loop=", packet)
+                if len(upload) == 0:
+                    print("Finished sending.")
+                    exit()
+            print("received data: {}: {}".format(ip, data.decode()))
+        except socket.timeout: #general socket timeout
+            print("Socket time out, server didn't respond in time.")
+            socket_timeouts += 1
+            if socket_timeouts == 3:
+                print("Socket time out limit reached, server didn't respond in time.")
+                exit()
+            continue
+        except socket.error:
+            print("Error! {}".format(socket.error))
+            exit()
+
 
 
 def get_client_id():
--- assignment1/udp/udp_server.py
+++ assignment1/udp/udp_server.py
@@ -1,21 +1,107 @@
+"""
+Implement a simple acknowledgement protocol that you assign unique sequence id for each package you send to the server.
+Once the package is received by the server, the server will send acknowledgement back to the client.
+In case of package lost, the client did not receive the acknowledgement back from the server,
+the client must resend the same package again until you get the acknowledgement.
+To control the package order, the client will never send the next package until it gets the acknowledegement for the previous one.
+"""
+
 import socket
 
 
 UDP_IP = '127.0.0.1'
 UDP_PORT = 4000
 BUFFER_SIZE = 1024
-MESSAGE = "pong"
+MESSAGE = "acknowledgment"
+RESEND = "resend"
+clients = {}
+
+def get_client_id(s):
+	count = 0
+	idx = s.find(":")
+	return s[0:idx]
+
+def get_packet(s):
+	idx = s.find(":")
+	return get_client_id(s[idx+1:])
+
+def get_data(s):
+	colon = s.find(":")
+	s = s[colon+1:]
+	colon = s.find(":")
+	return (s[colon+1:])
+
 
 def listen_forever():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(("", UDP_PORT))
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(("", UDP_PORT))
+	s.settimeout(2.5)
+	timeouts = 0
+	while True:
+		try:
+			data, ip = s.recvfrom(BUFFER_SIZE)
+			stripped = data.decode(encoding="utf-8").strip()
+			client_id = get_client_id(stripped)
+			print(client_id)
+			#first time client connects
+			if client_id not in clients:
+				clients[client_id] = [get_data(stripped)] #implicitly packet 0
+				s.sendto(MESSAGE.encode(), ip)
+				print(clients)
+			else:
+				print("stripped =", stripped)
+				print("get packet =", get_packet(stripped))
+				if int(get_packet(stripped)) == len(clients[client_id]):
+					print("else if correct packet")
+					clients[client_id].append(get_data(stripped))
+					s.sendto(MESSAGE.encode(), ip)
+				else: #packet sent was out of order, resend the requested packet
+					print("else resend")
+					s.sendto(RESEND.encode(), ip)
+		except (socket.timeout, socket.error) as e:
+			print(e)
+			timeouts += 1
+			if timeouts == 5:
+				print("timeout limit exceeded, no more clients connecting, exiting")
+				exit()
 
-    while True:
-        # get the data sent to us
-        data, ip = s.recvfrom(BUFFER_SIZE)
-        print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
-        # reply back to the client
-        s.sendto(MESSAGE.encode(), ip)
+
 
 
 listen_forever()
+
+
+
+#string id format:
+
+#clientid:packet#:data
+
+#only handles one client sending information to it in its lifetime
+# def listen_forever():
+# 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 	s.bind(("", UDP_PORT))
+# 	s.settimeout(2.5)
+# 	timeouts = 0
+# 	integrity = 0 #this is the parity check for packet loss
+# 	while True:
+# 		try:
+# 			# get the data sent to us
+# 			data, ip = s.recvfrom(BUFFER_SIZE)
+# 			# strip data
+# 			stripped = data.decode(encoding="utf-8").strip()
+# 			# parse out line number
+# 			check = stripped[0:parse_string(stripped)]
+
+# 			if int(check) == integrity + 1:
+# 				# reply back to the client
+# 				s.sendto(MESSAGE.encode(), ip)
+# 				timeouts = 0
+# 				integrity += 1
+# 			else:
+# 				s.sendto(RESEND.encode(), ip)
+# 		except (socket.timeout, socket.error, Exception) as e:
+# 			timeouts += 1
+# 			if timeouts == 7:
+# 				print("timeout limit exceeded, no more clients connecting, exiting")
+# 				exit()
+# 			print(str(e))
