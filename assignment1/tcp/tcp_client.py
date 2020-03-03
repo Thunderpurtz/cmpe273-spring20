3ae44883e053c8ff662d6bef63bc27dfddbd2316
Mason Qian <Thunderpurtz@Masons-MacBook-Pro-2.local>
2020-03-02 22:16:49 -0800

    finished tcp

--- assignment1/tcp/tcp_client.py
+++ assignment1/tcp/tcp_client.py
@@ -1,4 +1,6 @@
 import socket
+import time
+import sys
 
 
 TCP_IP = '127.0.0.1'
@@ -9,15 +11,21 @@ MESSAGE = "ping"
 def send(id=0):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(f"{id}:{MESSAGE}".encode())
+    #s.send(f"{id}:{MESSAGE}".encode())
+    s.send(f"Connected Client:{id}.".encode())
     data = s.recv(BUFFER_SIZE)
+    for _ in range(int(sys.argv[3])):
+        print(f"Sending data:{MESSAGE}")
+        s.send(f"Received data:{id}:{MESSAGE}".encode())
+        data = s.recv(BUFFER_SIZE)
+        print("Received data:", data.decode())
+        time.sleep(int(sys.argv[2]))
+
     s.close()
-    print("received data:", data.decode())
 
 
 def get_client_id():
-    id = input("Enter client id:")
+    id = sys.argv[1]
     return id
-
 
 send(get_client_id())
--- assignment1/tcp/tcp_server.py
+++ assignment1/tcp/tcp_server.py
@@ -1,26 +1,45 @@
 import socket
-
+import threading
+import sys
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
+all_connections = []
+all_addresses = []
 
 def listen_forever():
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
-
-    conn, addr = s.accept()
-    print(f'Connection address:{addr}')
-
     while True:
-        data = conn.recv(BUFFER_SIZE)
-        if not data: 
-            print('No data received.')
+        s.listen(1)
+        conn, addr = s.accept()
+        all_connections.append(conn)
+        all_addresses.append(addr)
+
+def process_clients():
+    while True:
+        for idx, conn in enumerate(all_connections):
+            data = conn.recv(BUFFER_SIZE)
+            if not data:
+                conn.close()
+                del all_connections[idx]
+                del all_addresses[idx]
+                continue
+            print(f"{data.decode()}")
+            conn.send("pong".encode())
+        if len(all_connections) < 1:
             break
-        print(f"received data:{data.decode()}")
-        conn.send("pong".encode())
 
-    conn.close()
 
-listen_forever()
+#thread to handle listening and to put connections in an array
+print(f"Server started at port {TCP_PORT}.")
+sys.stdout.flush()
+t1 = threading.Thread(target=listen_forever)
+t1.daemon = True
+t1.start()
+while True:
+    if len(all_connections) > 0:
+        process_clients()
